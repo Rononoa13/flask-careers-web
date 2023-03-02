1a7055dc8f1a76103874e1bf767a04a2afc19f44
Rononoa13 <sumitluitel42@gmail.com>
2023-03-03 00:01:47 +0545

    Sql alchemy engine config with sqlite3 and database return value in dict

--- app.py
+++ app.py
@@ -1,41 +1,20 @@
 from flask import Flask, jsonify, render_template
+import database
+
 
 app = Flask(__name__)
 
-JOBS = [
-    {
-        'id': 1,
-        'title': 'Data Analyst',
-        'location': 'USA, Miami',
-        'salary': '$64000 per year'
-    },
-    {
-        'id': 2,
-        'title': 'Data QA Engineer',
-        'location': 'USA, Newyork',
-        'salary': '$54000 per year'
-    },
-    {
-        'id': 3,
-        'title': 'Senior Data Engineer',
-        'location': 'Remote'
-    },
-    {
-        'id': 4,
-        'title': 'Senior Backend Engineer',
-        'location': 'Remote',
-        'salary': '$84000 per year'
-    }
-]
 
 @app.route("/")
 def index():
-    return render_template("home.html", jobs=JOBS, company_name="Bell")
+    jobs = database.load_jobs()
+    return render_template("home.html", jobs=jobs, company_name="Bell")
 
 
 @app.route("/api/jobs")
 def list_jobs():
-    return jsonify(JOBS)
+    jobs = database.load_jobs()
+    return jsonify(jobs)
 
 
 if __name__ == "__main__":
---
+++ database.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine, text
+
+# Turn on database engine
+engine = create_engine(f"sqlite:///jobscareer.db",
+    #                    connect_args={
+    #     "ssl": {
+    #         "ca": "/home/gord/client-ssl/ca.pem",
+    #         "cert": "/home/gord/client-ssl/client-cert.pem",
+    #         "key": "/home/gord/client-ssl/client-key.pem"
+    #     }
+    # }
+)
+
+def load_jobs():
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM jobs"))
+        result_dict = []
+        for row in result.all():
+            """
+            The results look like tuples/lists, but they are actually a special Row object 
+            (KeyedTuple for SQLAlchemy < 1.4). Use the _asdict() method to convert each row 
+            to a dict.
+            """
+            result_dict.append(row._asdict())
+        return result_dict
+
