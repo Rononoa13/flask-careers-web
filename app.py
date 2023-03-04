3562d18ca132e452cfd71099264ab61c3c087219
Rononoa13 <sumitluitel42@gmail.com>
2023-03-04 12:56:49 +0545

    Added dynamic pages for each job

--- app.py
+++ app.py
@@ -8,13 +8,21 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     jobs = database.load_jobs()
-    return render_template("home.html", jobs=jobs, company_name="Bell")
+    return render_template("home.html", jobs=jobs, company_name="Bells")
 
 
 @app.route("/api/jobs")
 def list_jobs():
     jobs = database.load_jobs()
     return jsonify(jobs)
+
+
+@app.route("/job/<id>")
+def show_job(id):
+    job = database.load_job(id)
+    if not job:
+        return "Not Found", 404
+    return render_template("job_page.html", company_name="Bells", job=job)
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine, text
-
+import json
 # Turn on database engine
 engine = create_engine(f"sqlite:///jobscareer.db",
     #                    connect_args={
@@ -24,3 +24,14 @@ def load_jobs():
             result_dict.append(row._asdict())
         return result_dict
 
+def load_job(id):
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM jobs WHERE id =:val"),
+                              parameters={"val": id}
+                              )
+        rows = result.all()
+        if len(rows) == 0:
+            return None
+        else:
+            return rows[0]._asdict()
+        
